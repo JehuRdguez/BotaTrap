cd35b04e9ee6e12763ddf973eb85bad3a78f10e1
JehuRdguez <101675453+JehuRdguez@users.noreply.github.com>
2024-01-21 21:44:24 -0600

    Commit inicial

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// Toasts
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
